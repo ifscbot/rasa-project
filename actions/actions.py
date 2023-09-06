09ba3ce306330dacbd3c3168a39a48e0820393bf
João Gabriel <joogps@gmail.com>
2023-09-06 16:39:08 -0300

    Atualizar fluxos

--- actions/actions.py
+++ actions/actions.py
@@ -27,25 +27,27 @@ class ActionProcessarTipoDocumento(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
+        documento = tracker.get_slot("documento")
+        print("Documento: " + documento)
+
+        tipo_documento = tracker.get_slot("tipo_documento")
+        print("Tipo de documento: " + tipo_documento)
+
+        nome_documento = ""
+
+        if documento == "atestado_matricula":
+            nome_documento = "Atestado de matrícula"
+        elif documento == "atestado_frequencia":
+            nome_documento = "Atestado de frequência"
+        else:
+            dispatcher.utter_message(text=f"Desculpe, não consegui entender que tipo de documento você quer. Você poderia tentar de novo?")
+            return []
+        
+        if tipo_documento == "digital":
+            dispatcher.utter_message(text=f"Certo. Para emitir o seu {nome_documento} a partir do SIGAA, siga os passos abaixo:")
+            dispatcher.utter_message(text=f"- Primeiro acesse o [SIGAA](https://sigaa.ifsc.edu.br) utilizando seu login e senha; Ao acessar o SIGAA, no menu superior, clique em \"Ensino\" e, em seguida, em \"{nome_documento}\".")
+            dispatcher.utter_message(text=f"- Então,  o seu {nome_documento} será aberto em uma nova janela. Você pode imprimi-lo ou salvá-lo através do botão “imprimir” no final da página.")
+        else:
+            dispatcher.utter_message(text=f"Beleza. Para receber o seu {nome_documento} impresso com carimbo e assinatura, você deve ir à secretaria do IFSC Campus Gaspar pessoalmente. O horário de atendimento é de segunda a sexta-feira, das 8h às 20h.")
+
         return []
-    
-
-class ActionSetLogado(Action):
-    def name(self) -> Text:
-        return "action_set_logado"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        return [SlotSet("logado", True)]
-
-class ActionSetLogadoFalse(Action):
-    def name(self) -> Text:
-        return "action_set_logado_false"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        return [SlotSet("logado", False)]
